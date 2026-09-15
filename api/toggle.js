bc8434731bf5176efc2139ca3512c2a3984b2a01
noeljakisa2-ai <noeljakisa2@gmail.com>
2026-09-15 18:03:55 +0300

    Delete api directory

--- api/toggle.js
+++
@@ -1,7 +0,0 @@
-import { kv } from '@vercel/kv';
-export default async function handler(req,res){
-let s=await kv.get('tempest_state')||{balance:10000,profit:0,trades:[],isAuto:true};
-s.isAuto=!s.isAuto;
-await kv.set('tempest_state',s);
-return res.json(s);
-}
